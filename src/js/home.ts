88c3f825f505cce27afce39001fa5dca3820d3ac
Brian Norris <bnorris@marlinnetwork.com>
2023-04-07 16:39:09 -0500

    vue app example

--- src/js/home.ts
+++ src/js/home.ts
@@ -1,3 +1,9 @@
 import '../scss/home.scss';
 
 import './scripts/home';
+
+// https://parceljs.org/languages/vue/
+import { createApp } from "vue";
+import App from "./scripts/vue-app.vue";
+const app = createApp(App);
+app.mount("#app");
---
+++ src/js/scripts/vue-app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello {{ name }}!</div>
+</template>
+
+<script lang="ts">
+export default {
+	setup() {
+		const name: string = 'Brian';
+		return {
+			name
+		};
+	},
+};
+</script>
